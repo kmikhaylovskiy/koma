a55690e6d9723ee79f51dfbe05852b00da054a82
kmikhaylovskiy <konstantin.mikhailovsky@gmail.com>
2025-03-21 09:56:50 +0000

    change gitignore

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "hiasvm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file(".ssh/id_rsa.pub")
+    public_key = file(".ssh/id_rsa.pem.pub")
   }
 
   os_disk {
